3929d5a6d4bd7d6e598ab2b7a0b52c3dcbbbdebe
Murzbul <natanaelrusso18@hotmail.com>
2018-03-20 11:14:08 -0300

    View delete.blade.php modified with bootstrap 4 and separate css

--- resources/views/layouts/css.blade.php
+++ resources/views/layouts/css.blade.php
@@ -1,17 +1,4 @@
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet">
-
-<style media="screen">
-    .navbar
-    {
-        padding: 0.3rem;
-        padding-left: 0.3rem;
-        padding-left: 2rem;
-    }
-    #logout
-    {
-        cursor: pointer;
-    }
-
-</style>
+<link rel="stylesheet" href="{{ URL::asset('css/index.css') }}">
--- resources/views/layouts/delete.blade.php
+++ resources/views/layouts/delete.blade.php
@@ -1,18 +1,20 @@
-<!-- Modal -->
-<div id="delete_modal_{{ $id }}" class="modal fade" role="dialog">
-  <div class="modal-dialog">
 
-    <!-- Modal content-->
+<div  id="delete_modal_{{ $id }}" class="modal" tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">{{ $title}}</h4>
+        <h5 class="modal-title">{{ $title }}</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
       </div>
       <div class="modal-body">
         <p>{{ $content }}</p>
-        <p>{{$route}}</p>
-          <a class="btn btn-success" href="{{ route( $route, ['id' => $id]) }}">Si</a>
-          <a type="button" class="btn btn-default" data-dismiss="modal">No</a>
+        <p>{{ $user->name }}</p>
+      </div>
+      <div class="modal-footer">
+        <a class="btn btn-success" href="{{ route( $route, ['id' => $id]) }}">Si</a>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">No</button>
       </div>
     </div>
   </div>
